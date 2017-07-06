6f0e26aafdd1d01593a89477ff5775f14e47fbfc
ytsai43 <ytsai43@asu.edu>
2017-07-06 16:41:50 -0700

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,12 +1,13 @@
+%cd C:\Users\ytsai\Desktop\python
 %config IPCompleter.greedy=True
-
+%reset
 import numpy as np
 import copy
 from random import*
 from math import*
 
 def partition(range_list, depth=0):
-    
+
     k = len(range_list[0]) # cal number of dimensions
     axis = depth % k # Select axis based on depth so that axis cycles through all valid values  
     if abs(range_list[0][axis][1] - range_list[0][axis][0]) < epsilon:  #Set the condition that the range cann't be divided 
@@ -16,8 +17,8 @@ def partition(range_list, depth=0):
             axis = (axis + 1)%k
         if axis == axis_hat:
             return (range_list,depth)
-        
-    
+
+
     #print(cuttingPoint)
     left_range_list = copy.deepcopy(range_list)
     right_range_list = copy.deepcopy(range_list) 
@@ -27,7 +28,7 @@ def partition(range_list, depth=0):
         right_range_list[j][axis] = (cuttingPoint,range_list[j][axis][1])
     range_list = left_range_list+ right_range_list
     return (range_list, depth+1) 
-    
+
 
 def criteria(region_list, iteration): 
     numberofSubregion = len(region_list)
@@ -40,10 +41,11 @@ def criteria(region_list, iteration):
             for d in range(dim):                
                 X_matrix[m,n,d] = np.random.uniform(region_list[m][d][0],region_list[m][d][1])
     # f(X) with noise
-    fofx = 0.01 * X_matrix[:,:,0]**2 + X_matrix[:,:,1]**2-100
+    fofx = (1-X_matrix[:,:,0])**2 + 100*((X_matrix[:,:,1]-X_matrix[:,:,0]**2))**2
+    #fofx = np.sin(X_matrix[:,:,0])
     tempfofx = fofx.repeat(R)
-    noise = np.random.normal(mean,sigma,N*R*numberofSubregion)
-    #noise = 0
+    #noise = np.random.normal(mean,sigma,N*R*numberofSubregion)
+    noise = 0
     fofx = tempfofx+noise
     hatFofx = fofx.reshape(numberofSubregion,N,R)
     #print('hatFox=',hatFofx)
@@ -57,10 +59,10 @@ def criteria(region_list, iteration):
     best_x_solution = X_matrix[bestRegion][bestPointinbestRegion]  #final optimal solution x
     worstPointwithNoise = hatFofx[bestRegion][bestPointinbestRegion][np.argmax(hatFofx[bestRegion][bestPointinbestRegion])]
     #print('worstPointwithNoise=',worstPointwithNoise)
-    
+
     notbestRegion = np.argmin(avgfofx,axis = 1)
     #print('notbestRegion',notbestRegion)
-    
+
     remainRegion = []
     remainRegion.append(region_list[bestRegion])
     for i in range(numberofSubregion):
@@ -71,27 +73,58 @@ def criteria(region_list, iteration):
             remainRegion.append(region_list[i])
     return(remainRegion,best_x_solution,best_object_value)
 
-
-range_list=[[(2,50),(-50,50)]]
+####### find threshold value
+fxstar = 0
 delta = 0.01 #clossness parameter
-alpha = 0.25 #error rate
-epsilon = 0.01 #condition to terminate subregion with continuous case 
-mean,sigma = 0,0.0001   # distribution of noise
-#M = 2 #number of subregions
+alpha = 0.5 #error rate
+epsilon = 4 #condition to terminate subregion with continuous case 
+mean,sigma = 0,1   # distribution of noise
 
-newRange_list,depth = partition(range_list)
-bestRegion_list = range_list[:]
-newRange_list.sort()
-range_list.sort()
-iteration = 1
-while(newRange_list != range_list):
-    range_list,best_x,best_object_value = criteria(newRange_list,iteration)
-    newRange_list,depth = partition(range_list,depth)
+n_sample = 10000000
+x_set = np.zeros((n_sample,2))
+x_set[:,0]= np.random.uniform(-2,2,n_sample) 
+x_set[:,1]= np.random.uniform(-2,2,n_sample) 
+fx = (1-x_set[:,0])**2 + 100*((x_set[:,1]-x_set[:,0]**2))**2
+fx.sort()
+target_threshold = fx[int(n_sample*delta)]
+########
+
+ps = 0
+test_region = []
+for i in range(1000):
+    
+    range_list=[[(-2,2),(-2,2)]]
+
+    
+    newRange_list,depth = partition(range_list)
     bestRegion_list = range_list[:]
     newRange_list.sort()
-    range_list.sort()   
-    iteration += 1
-#final output is x(1)(1),hatfofx,remainging subregion
-print(best_x,best_object_value)    
-print(bestRegion_list)
+    range_list.sort()
+    iteration = 1
+
+    while(newRange_list != range_list):
+        range_list,best_x,best_object_value = criteria(newRange_list,iteration)
+        newRange_list,depth = partition(range_list,depth)
+        bestRegion_list = range_list[:]
+        newRange_list.sort()
+        range_list.sort()   
+        iteration += 1
+    #final output is x(1)(1),hatfofx,remainging subregion
+    #print(best_x,best_object_value)    
+    #print(bestRegion_list)
+    test_region.append(bestRegion_list[0])
+    dis_f = abs(best_object_value - fxstar)
+    #dis_x = abs(((best_x[0]-1)**2 + (best_x[1]-1)**2)**1/2)
+    
+    if (best_object_value-target_threshold < 0):
+
+        ps = ps + 1
+    print(dis_f)
+    #print(best_object_value)
+print(ps/1000)
+#test_region.sort()
+#print(test_region)
+    
+  
+
     
